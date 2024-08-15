daede7402294365e138b0fcc9effc023e9739ca2
Abdi Bacha <contactabdi.bacha@gmail.com>
2024-08-15 13:28:19 +0300

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,9 +14,18 @@ const Hero = () => {
         <div className="hero-text">
           <h1>Hey, I'm Abdi 👋</h1>
           <p>
-            I'm a software developer based in Addis Ababa, Ethiopia. I
+            {/* I'm a software developer based in Addis Ababa, Ethiopia. I
             specialize in building (and occasionally designing) exceptional
-            websites, applications, and everything in between.
+            websites, applications, and everything in between. */}
+            A <span> MERN </span> Full Stack Developer with a solid foundation in building and
+            maintaining dynamic web applications. Proficient in MongoDB,
+            Express.js, React.js, and Node.js, I bring a comprehensive skill set
+            that spans both <span> front-end and back-end </span>  development. Driven by a
+            passion for continuous learning and professional growth, I am
+            committed to delivering high-quality, scalable solutions that meet
+            client needs. Adaptable and self-motivated, I excel in both
+            collaborative and remote work environments, consistently managing
+            time and tasks to achieve project goals efficiently.
           </p>
           <div className="social-icons">
             <a
