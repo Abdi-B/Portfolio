0542546a1abc77bd29fa45ca6ea0aa750f9e9497
Abdi Bacha <contactabdi.bacha@gmail.com>
2024-08-16 11:15:08 +0300

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
         <h2>About Me</h2>
         <div className="flex-about">
           <div className="about-text">
-            <p>
+            {/* <p>
               As a developer, I have always been passionate about creating
               elegant and effective solutions to complex problems. I have a
               strong foundation in software development, with a focus on web
@@ -24,7 +24,72 @@ const About = () => {
               Vue.js, Node.js, and Laravel. I am always eager to learn and
               explore new technologies, and I am constantly seeking out
               opportunities to improve my skills and knowledge.
+            </p> */}
+            <p>
+              As a proficient MERN full-stack developer, I am committed to
+              crafting robust and scalable web applications that meet the
+              highest standards of performance and security. My expertise spans
+              across both front-end and back-end development, ensuring seamless
+              integration and functionality. I am adept at working in
+              collaborative environments, driving projects from concept to
+              deployment with a focus on innovation and efficiency.
             </p>
+
+            <div className="competency">
+            <h3>Key Competencies:</h3>
+
+              <p>
+                {" "}
+                <span>Full-Stack Development: </span> Build and maintain
+                full-stack web applications using the MERN stack.
+              </p>
+              <p>
+                {" "}
+                <span>Team Collaboration: </span> Work closely with
+                cross-functional teams to define, design, and deliver new
+                features.
+              </p>
+              <p>
+                {" "}
+                <span>API Implementation: </span> Develop and integrate RESTful
+                APIs with front-end components.
+              </p>
+              <p>
+                {" "}
+                <span>Code Quality:</span> Write clean, scalable code with
+                comprehensive documentation.
+              </p>
+              <p>
+                {" "}
+                <span>Optimization: </span> Optimize applications for maximum
+                speed and scalability.
+              </p>
+              <p>
+                {" "}
+                <span>Code Review & Debugging: </span> Conduct thorough code
+                reviews, testing, and debugging.
+              </p>
+              <p>
+                {" "}
+                <span>Security: </span> Implement robust authentication,
+                authorization, and error handling practices.
+              </p>
+              <p>
+                {" "}
+                <span>Load Balancing: </span> Utilize load balancers to enhance
+                application performance.
+              </p>
+              <p>
+                {" "}
+                <span>Middleware: </span> Integrate middleware for added
+                functionality and control.
+              </p>
+              <p>
+                {" "}
+                <span>Validation & Routing: </span> Ensure proper validation and
+                efficient routing mechanisms.
+              </p>
+            </div>
           </div>
           <div className="about-img">
             <Image
