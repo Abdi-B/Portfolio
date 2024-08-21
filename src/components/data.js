426999f3fd4062b9812f4ed5c61ded9fe75cd890
Abdi Bacha <contactabdi.bacha@gmail.com>
2024-08-21 16:50:45 +0300

    update

--- src/components/data.js
+++ src/components/data.js
@@ -1,33 +1,33 @@
 export const projectData = [
   {
     id: 1,
-    title: "Company Profile",
+    title: "Wadaf Ethiopia Mining PLC",
     description:
       "A company profile built in Next.js, Typescript and Tailwind css.",
-    gitHubLink: "https://github.com/olawanlejoel/Todo-List-App",
+    gitHubLink: " https://github.com/Abdi-B/wadafmining",
     deployedLink: "https://wadafmining2.vercel.app/",
   },
   {
     id: 2,
-    title: "Coop file Sharing",
+    title: "Cooperative Bank  file Sharing",
     description:
       "This projects is built in MERN Full Stack. Its purpose is to manage a files in a simple way and to get books provided by the company",
-    gitHubLink: "https://github.com/olawanlejoel/Book-Library",
+    gitHubLink: "https://github.com/Abdi-B/coopFileMangement.git",
     deployedLink: "https://coop-file-sharing.vercel.app/",
   },
   {
     id: 3,
     title: "ATM & POS Management System",
     description:
-      "Helps you generate quotes from about 1600 quotes written by different authors . Quotes are automatically copied to your clipboards.",
-    gitHubLink: "https://github.com/olawanlejoel/random-quote-generator",
-    deployedLink: "https://wadafmining2.vercel.app/",
+      "A comprehensive solution designed to efficiently Manage, Create, Explore and View operations related to ATMs and POS devices. Generating detailed reports and maintaining logs, ensuring accurate tracking and monitoring of all activities",
+    gitHubLink: "https://github.com/Abdi-B/TMS.git",
+    deployedLink: "https://coop-file-sharing.vercel.app",
   },
   {
     id: 4,
-    title: "Password Generator",
+    title: "Ecommerce",
     description:
-      "Helps you generates random passwords, you can select what you want your password to entail and also you can copy generated password to clipboard.",
+      "Developed a dynamic eCommerce platform using React.js and Redux, enabling efficient state management and seamless user experience. The application features product browsing, cart management, and secure checkout, optimized for performance and scalability.",
     gitHubLink: "https://github.com/olawanlejoel/Password-Generator",
     deployedLink: "https://wadafmining2.vercel.app/",
   },
