624f658d2ce9eb61c108a7529f348db831275978
Abdi Bacha <contactabdi.bacha@gmail.com>
2024-08-22 14:42:51 +0300

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,10 +15,11 @@ const Hero = () => {
           <div className="hero-text">
             <h1>Hey, I'm Abdi 👋</h1>
             <p>
-              A <span> MERN </span> Full Stack Developer with a solid foundation in building and
-              maintaining dynamic web applications. Proficient in MongoDB,
-              Express.js, React.js, and Node.js, I bring a comprehensive skill set
-              that spans both <span> front-end and back-end </span>  development. Driven by a
+              A <span> MERN </span> Full Stack Developer with a solid foundation
+              in building and maintaining dynamic web applications. Proficient
+              in MongoDB, Express.js, React.js, and Node.js, I bring a
+              comprehensive skill set that spans both{" "}
+              <span> front-end and back-end </span> development. Driven by a
               passion for continuous learning and professional growth, I am
               committed to delivering high-quality, scalable solutions that meet
               client needs. Adaptable and self-motivated, I excel in both
@@ -76,25 +77,57 @@ const Hero = () => {
           <i className="devicon-nodejs-plain colored"></i>
           <i className="devicon-mongodb-plain colored"></i>
         </div> */}
-        
+
         <div className="hero-img">
-          
-            <Image
+          <Image
+            src="/images/mongodb.jpg"
+            className="mongodb-img mern-img"
+            width={150}
+            height={100}
+            alt="developer" 
+            
+          />
+          <Image 
+            src="/images/Express3.png"
+            className="express-img mern-img"
+            width={100}
+            height={100}
+            alt="developer"
+          />
+          <Image
+            src="/images/Reactjs2.png"
+            className="react-img mern-img"
+            width={100}
+            height={100}
+            alt="developer"
+          />
+          <Image
+            src="/images/Nodejs.png"
+            className="mern-img"
+            width={100}
+            height={100}
+            alt="developer"
+          />
+
+          {/* <FontAwesomeIcon icon="fa-brands fa-react" />
+        <i class="fa-brands fa-react fa-spin"></i>
+        <i class="fa-brands fa-node fa-fade"></i> */}
+
+          {/* <Image
               src="/images/MERN.png"
               className="mern-img"
               width={500}
               height={500}
               alt="developer"
-            />
-            {/* <Image
+            /> */}
+          {/* <Image
               src="/images/dev.jpg"
               className="home-img"
               width={500}
               height={500}
               alt="developer"
             />  */}
-
-          </div>
+        </div>
       </div>
     </section>
   );
