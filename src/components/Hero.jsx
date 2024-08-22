40f202d69dae180926c7407d525a65fba0b2e95a
Abdi Bacha <contactabdi.bacha@gmail.com>
2024-08-22 10:04:32 +0300

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
             </p>
             <div className="social-icons">
               <a
-                href="https://twitter.com/olawanle_joel"
+                // href="https://twitter.com/olawanle_joel"
                 aria-label="Twitter"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -35,7 +35,7 @@ const Hero = () => {
                 <i className="fa-brands fa-twitter"></i>
               </a>
               <a
-                href="https://github.com/olawanlejoel"
+                // href="https://github.com/olawanlejoel"
                 aria-label="GitHub"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -43,23 +43,39 @@ const Hero = () => {
                 <i className="fa-brands fa-github"></i>
               </a>
               <a
-                href="https://www.linkedin.com/in/olawanlejoel/"
+                // href="https://www.linkedin.com/in/olawanlejoel/"
                 aria-label="LinkedIn"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 <i className="fa-brands fa-linkedin"></i>
               </a>
+              <a
+                // href="https://www.linkedin.com/in/olawanlejoel/"
+                aria-label="Telegram"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa-brands fa-telegram"></i>
+              </a>
+              <a
+                // href="https://www.linkedin.com/in/olawanlejoel/"
+                aria-label="WhatsApp"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa-brands fa-whatsapp"></i>
+              </a>
             </div>
           </div>
         </div>
-        <div className="tech-icons">
+        {/* <div className="tech-icons">
           <i className="devicon-react-original colored"></i>
           <i className="devicon-nextjs-original colored"></i>
           <i className="devicon-express-original colored"></i>
           <i className="devicon-nodejs-plain colored"></i>
           <i className="devicon-mongodb-plain colored"></i>
-        </div>
+        </div> */}
         
         <div className="hero-img">
           
